e69983c5cc8d6160e9c886e2f2949754a1f927cf
Josh Cirre <joshcirre@gmail.com>
2024-12-13 15:03:24 -0700

    🦍 CHANGE TITLE 🦍

--- resources/views/components/layouts/main.blade.php
+++ resources/views/components/layouts/main.blade.php
@@ -12,7 +12,7 @@ new class extends Component {};
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="csrf-token" content="{{ csrf_token() }}" />
 
-        <title>{{ config("app.name", "My Laravel App") }}</title>
+        <title>🦍 MAKE TEXT STRONK 🦍</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.googleapis.com" />
